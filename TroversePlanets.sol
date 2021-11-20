458f172339f3d1c4c6407d2cf89aed1f26a881df
TroverseDev <94727897+TroverseDev@users.noreply.github.com>
2021-11-19 18:58:51 -0800

    Spacings updated

--- TroversePlanets.sol
+++ TroversePlanets.sol
@@ -43,46 +43,46 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
     constructor() ERC721("Troverse Planets", "PLANET") { }
 
 
-	/**
-	 * @dev Get number of reserved NFTs which will be used for gifts or giveaways
-	 */
+    /**
+    * @dev Get number of reserved NFTs which will be used for gifts or giveaways
+    */
     function getTotalReservedMints() private view returns(uint) {
         return _reservedMints > RESERVED_PLANETS ? _reservedMints : RESERVED_PLANETS;
     }
 
-	/**
-	 * @dev Get number of NFTs which are minted or reserved
-	 */
+    /**
+    * @dev Get number of NFTs which are minted or reserved
+    */
     function getMintedOrReservedPlanets() private view returns(uint) {
         return totalSupply() + getTotalReservedMints() - _reservedMints;
     }
 
-	/**
-	 * @dev Get remaining number of NFTs which can be minted in the public sale
-	 */
+    /**
+    * @dev Get remaining number of NFTs which can be minted in the public sale
+    */
     function getRemainingPlanets() public view returns(uint) {
         return TOTAL_PLANETS - getMintedOrReservedPlanets();
     }
 
-	/**
-	 * @dev Check if the public sale auction is sold-out
-	 */
+    /**
+    * @dev Check if the public sale auction is sold-out
+    */
     function isAuctionSoldOut() private view returns(bool) {
         return getRemainingPlanets() <= 0;
     }
 
-	/**
-	 * @dev List all eligible addresses for the pre-sale
-	 */
+    /**
+    * @dev List all eligible addresses for the pre-sale
+    */
     function PreSaleList() public view returns (address[] memory) {
         return _preSaleList.values();
     }
 
-	/**
-	 * @dev Add eligible addresses for the pre-sale
-     * @param list List of addresses to be added for the pre-sale
-     * @param limit Number of eligible mints per address
-	 */
+    /**
+    * @dev Add eligible addresses for the pre-sale
+    * @param list List of addresses to be added for the pre-sale
+    * @param limit Number of eligible mints per address
+    */
     function addPreSaleList(address[] memory list, uint limit) public onlyOwner {
         for (uint i; i < list.length; i++) {
             if (!_preSaleList.contains(list[i])) {
@@ -92,19 +92,19 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
         }
     }
 
-	/**
-	 * @dev Returns how many NFTs can be minted during pre-sale by an address
-     * @param owner The target address
-	 */
+    /**
+    * @dev Returns how many NFTs can be minted during pre-sale by an address
+    * @param owner The target address
+    */
     function getPreSaleCount(address owner) public view returns (uint) {
         return _preSaleCounts[owner];
     }
 
-	/**
-	 * @dev Mint for gifts and giveaways
-     * @param to The target address
-     * @param count The number of NFTs to be minted
-	 */
+    /**
+    * @dev Mint for gifts and giveaways
+    * @param to The target address
+    * @param count The number of NFTs to be minted
+    */
     function reserveMint(address to, uint count) public onlyOwner {
         uint lastTotalSupply = totalSupply();
         require(lastTotalSupply + count <= TOTAL_PLANETS);
@@ -115,11 +115,11 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
         }
     }
 
-	/**
-	 * @dev Try to mint NFTs during the pre-sale if the sender is whitelisted
-     *      Any extra funds will be transferred back to the sender's address
-     * @param count The number of NFTs to be minted
-	 */
+    /**
+    * @dev Try to mint NFTs during the pre-sale if the sender is whitelisted
+    *      Any extra funds will be transferred back to the sender's address
+    * @param count The number of NFTs to be minted
+    */
     function preSale(uint count) public payable nonReentrant {
         require(isPreSaleOpen(), "Pre sale is currently closed");
         require(!isAuctionSoldOut(), "Sold-out!");
@@ -141,12 +141,12 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
         }
     }
 
-	/**
-	 * @dev Try to mint NFTs during the public sale
-     *      During the dutch auction phase, credits can be considered for future mints until it has been sold-out
-     *      Any extra funds will be transferred back to the sender's address
-     * @param count The number of NFTs to be minted
-	 */
+    /**
+    * @dev Try to mint NFTs during the public sale
+    *      During the dutch auction phase, credits can be considered for future mints until it has been sold-out
+    *      Any extra funds will be transferred back to the sender's address
+    * @param count The number of NFTs to be minted
+    */
     function sale(uint count) public payable nonReentrant {
         require(isSaleOpen(), "Sale is currently closed");
         require(!isAuctionSoldOut(), "Sold-out!");
@@ -190,62 +190,62 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
         }
     }
 
-	/**
-	 * @dev Get the refundable amount in the auction for an address
-     * @param owner The target address
-	 */
+    /**
+    * @dev Get the refundable amount in the auction for an address
+    * @param owner The target address
+    */
     function getRemainingCredit(address owner) public view returns(uint) {
         return credits[owner] - getCreditRefundPrice() * creditCount[owner];
     }
 
-	/**
-	 * @dev Get the refundable amount in the auction for all addresses
-	 */
+    /**
+    * @dev Get the refundable amount in the auction for all addresses
+    */
     function getTotalRemainingCredits() public view returns(uint) {
         return _totalCredits - getCreditRefundPrice() * _totalCreditCount;
     }
 
     /**
-     * @dev Set pre-sale state
-     * @param state New state
-     */
+    * @dev Set pre-sale state
+    * @param state New state
+    */
     function setPreSaleOpen(bool state) public onlyOwner {
         _preSaleOpen = state;
     }
 
     /**
-     * @dev Check if the pre-sale is started
-     */
+    * @dev Check if the pre-sale is started
+    */
     function isPreSaleOpen() public view returns(bool) {
         return _preSaleOpen;
     }
 
     /**
-     * @dev Check if the sale is started
-     */
+    * @dev Check if the sale is started
+    */
     function isSaleOpen() public view returns(bool) {
         return block.timestamp >= auctionStartTime && auctionStartTime > 0;
     }
 
     /**
-     * @dev Set the auction start time
-     * @param time Start time
-     */
+    * @dev Set the auction start time
+    * @param time Start time
+    */
     function setSaleTime(uint time) public onlyOwner {
         auctionStartTime = time;
     }
     
-	/**
-	 * @dev Calculate auction price on current time.
-	 */
+    /**
+    * @dev Calculate auction price on current time.
+    */
     function getAuctionPrice() public view returns(uint) {
         return getPriceOnTime(block.timestamp);
     }
 	
-	/**
-	 * @dev Calculate auction price with a timestamp
-     * @param time The target timestamp
-	 */
+    /**
+    * @dev Calculate auction price with a timestamp
+    * @param time The target timestamp
+    */
 	function getPriceOnTime(uint time) public view returns(uint) {
 		if(time < auctionStartTime) {
 			return SALE_INITIAL_PRICE;
@@ -261,24 +261,24 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
 		return SALE_INITIAL_PRICE - (currentRange * SALE_PRICE_DROP);
 	}
 
-	/**
-	 * @dev Get the auction refund price at this moment
-	 */
+    /**
+    * @dev Get the auction refund price at this moment
+    */
     function getCreditRefundPrice() public view returns(uint) {
         return isAuctionSoldOut() ? _lastAuctionSalePrice : getAuctionPrice();
     }
 
-	/**
-	 * @dev Check if the auction refund price is finalized
-	 */
+    /**
+    * @dev Check if the auction refund price is finalized
+    */
     function isAuctionPriceFinalized() internal view returns(bool) {
          return isAuctionSoldOut() || getAuctionPrice() <= SALE_ENDING_PRICE;
     }
 
-	/**
-	 * @dev List the tokens owned by an address
-     * @param owner The target address
-	 */
+    /**
+    * @dev List the tokens owned by an address
+    * @param owner The target address
+    */
     function tokensOfOwner(address owner) public view returns (uint[] memory) {
         uint count = balanceOf(owner);
         uint[] memory ids = new uint[](count);
@@ -289,23 +289,23 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
     }
 
     /**
-     * @dev Update the metadata with a new URI
-     * @param newBaseURI New BaseURI
-     */
+    * @dev Update the metadata with a new URI
+    * @param newBaseURI New BaseURI
+    */
     function setBaseURI(string memory newBaseURI) public onlyOwner {
         baseURI = newBaseURI;
     }
 
     /**
-     * @dev See {IERC721Metadata-_baseURI}.
-     */
+    * @dev See {IERC721Metadata-_baseURI}.
+    */
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
 
     /**
-     * @dev Refund the remaining ethereum balance if the auction price is finalized
-     */
+    * @dev Refund the remaining ethereum balance if the auction price is finalized
+    */
     function refundRemainingCredit() public payable nonReentrant {
         require(isAuctionPriceFinalized(), "Auction price is not finalized yet!");
         require(_creditOwners.contains(msg.sender), "Not a credit owner!");
@@ -321,19 +321,19 @@ contract TroversePlanets is ERC721Enumerable, Ownable, ReentrancyGuard {
     }
     
     /**
-     * @dev Withdraw a specific amount of funds from the smart contract to an address
-     * @param to The target address
-     * @param amount Amount to be withdrawn
-     */
+    * @dev Withdraw a specific amount of funds from the smart contract to an address
+    * @param to The target address
+    * @param amount Amount to be withdrawn
+    */
     function withdraw(address to, uint amount) public payable onlyOwner {
         require(to != address(0));
         require(payable(to).send(amount));
     }
 
     /**
-     * @dev Withdraw all eligible funds to an address
-     * @param to The target address
-     */
+    * @dev Withdraw all eligible funds to an address
+    * @param to The target address
+    */
     function withdrawAll(address to) public payable onlyOwner {
         uint toWithdrawFunds = address(this).balance - getTotalRemainingCredits();
         require(toWithdrawFunds > 0);
